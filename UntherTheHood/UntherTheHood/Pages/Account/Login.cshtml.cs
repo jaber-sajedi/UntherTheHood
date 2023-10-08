378d4a164328e1e5b3ffc21d73ef82ee47c0e243
jaber-sajedi <83576757+jaber-sajedi@users.noreply.github.com>
2023-10-08 12:58:34 +0330

    Start Project For Learning Login

---
+++ UntherTheHood/UntherTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,54 @@
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UntherTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+            
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid) return Page();
+
+            //Verify the Credential
+            if (Credential.UserName=="admin" && Credential.Password=="password")
+            {
+                //Creating the security context
+                var Claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "Admin"),
+                    new Claim(ClaimTypes.Email, "Jaber.sajedi@gmail.com")
+                };
+                var identity=new ClaimsIdentity(Claims,"MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+
+            }
+            return Page();
+        }
+
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display( Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+    }
+}
